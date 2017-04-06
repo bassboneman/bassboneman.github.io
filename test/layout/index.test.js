e3440b9e94ddaed41bdffb21d930ccec491a41af
Joshua Brown <josh@bassbone.com>
2017-04-06 15:38:24 -0500

    More removal

--- test/layout/index.test.js
+++
@@ -1,6 +0,0 @@
-load("init.js");
-
-testOnAllDevices("Product Page", "/product_page/", function (driver, device) {
-    checkLayout(driver, "test/specs/index.spec", device.tags);
-});
-
--- test/specs/all.spec
+++
@@ -1,3 +0,0 @@
-Home page on a small mobile device
-    http://localhost:4000    320x600
-        check   index.spec --include "mobile,all" --exclude "nomobile"
